fec7b53c8427d4e372eb797c76438dc849fcae34
frank-mck <fmckenna56330@gmail.com>
2023-09-04 12:32:21 +0100

    feat: add ability to rate brand

--- src/brands/Brand.tsx
+++ src/brands/Brand.tsx
@@ -1,13 +1,33 @@
+import { useState } from "react";
+import ThumbDown from "../icons/ThumbDown";
+import ThumbUp from "../icons/ThumbUp";
+
 export default function Brand({ data, state }) {
   const { 
     id,
     fields
   } = data;
+  const [onHover, setOnHover] = useState(false);
+  const [rating, setRating] = useState(localStorage.getItem(id));
   const thumbnail = fields?.images?.[0]?.thumbnails?.full;
   const showBrand = fields.tags.includes(state?.category.toLowerCase()) || state?.category === 'ALL';
-console.log(data)
+
+  const likeBrand = () => {
+    localStorage.setItem(id, "liked")
+    setRating("liked")
+  }
+
+  const dislikeBrand = () => {
+    localStorage.setItem(id, "disliked")
+    setRating("disliked")
+  }
+
   return (
-    <div className={`${showBrand ? '' : 'hidden'} h-[261px] grid grid-cols-[241px,1fr] text-sm bg-white border border-black w-full rounded-[20px] overflow-hidden`}>
+    <div onMouseOut={() => setOnHover(false)} onMouseOver={() => setOnHover(true)} className={`${showBrand ? '' : 'hidden'} h-[261px] relative grid grid-cols-[241px,1fr] text-sm bg-white border border-black w-full rounded-[20px] overflow-hidden`}>
+      {onHover ? (<div className="absolute top-0 right-0 flex">
+        <button className={`${rating === "disliked" ? 'bg-[#9EFF70]' : ''}`} onClick={dislikeBrand}><ThumbDown /></button>
+        <button className={`${rating === "liked" ? 'bg-[#9EFF70]' : ''}`} onClick={likeBrand}><ThumbUp /></button>
+      </div>) : null}
     <div className="h-[261px] overflow-hidden">
       <img className="w-full h-full object-cover" src={thumbnail?.url} alt="Thumbnail" />
     </div>
