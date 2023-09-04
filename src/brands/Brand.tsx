49622670cc63a86ffbefa511024a8aca9e06518a
frank-mck <fmckenna56330@gmail.com>
2023-09-04 12:46:33 +0100

    feat: add website link

--- src/brands/Brand.tsx
+++ src/brands/Brand.tsx
@@ -37,8 +37,17 @@ export default function Brand({ data, state }) {
           return <div key={id + tag} className="px-2 flex font-medium text-base items-center justify-center rounded-full h-[22px] border border-black">{tag}</div>
         })}
       </div>
-      <h2 className="font-normal text-[40px] my-3">{fields?.name}</h2>
+      <h2 className="font-normal text-[40px] my-4">{fields?.name}</h2>
       <p className="w-full">{fields?.description}</p>
+      <div className="flex items-center gap-4 mt-[17px]">
+        <div className="cursor-pointer flex items-center justify-center w-48 h-[34px] bg-lime-300 rounded-[50px] border border-black">
+          SEE DETAILS
+          <svg className="ml-2" width="19" height="17" viewBox="0 0 19 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0.938782 8.36956L18.4069 8.36956M18.4069 8.36956C12.6678 8.36956 10.9939 1.43478 10.9939 7.31026e-07M18.4069 8.36956C12.6678 8.36956 10.9939 13.8696 10.9939 16.5" stroke="black" strokeWidth="2"/>
+          </svg>
+        </div>
+         <a className="italic text-[#004D43] font-medium" href={fields?.website}>{fields?.website.match(/(\w+)\.\w{2,}(\/|\?|$)/)[1].toUpperCase()}</a>
+      </div>
     </div>
   </div>
   )
